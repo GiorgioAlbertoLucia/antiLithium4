ff3e96214816664c9a8196e4b8b6fda6ec6ae402
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2024-03-07 16:05:36 +0100

    workflow launcher

---
+++ MCWorkflowLauncher/launch_task.sh
@@ -0,0 +1,32 @@
+LOGFILE="output.log"
+CONF="-b --configuration json://configuration_mc.json"
+
+
+o2-analysis-lf-lithium4analysis $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
+
+    # converters
+    o2-analysis-tracks-extra-converter $CONF|
+    o2-analysis-bc-converter $CONF|
+
+    o2-analysis-timestamp $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
+    o2-analysis-event-selection $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
+    o2-analysis-track-propagation $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
+    o2-analysis-trackselection $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
+    o2-analysis-multiplicity-table $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
+    o2-analysis-pid-tpc-base $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
+    o2-analysis-pid-tpc-full $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
+    o2-analysis-pid-tof-base $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
+    o2-analysis-pid-tof-full $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000 --aod-writer-json OutputDirector_mc.json > $LOGFILE
+
+
+# report the status of the workflow
+rc=$?
+if [ $rc -eq 0 ]; then
+    echo "Workflow finished successfully"
+else
+    echo "Error: Workflow failed with status $rc"
+    echo "Check the log file for more details: $LOGFILE"
+    exit $rc
+fi
+
+    
