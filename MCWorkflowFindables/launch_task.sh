50a495176ad6bb845d83131028a98f85817e40d4
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2024-04-30 19:12:10 +0200

    configuration file changes

--- MCWorkflowFindables/launch_task.sh
+++ MCWorkflowFindables/launch_task.sh
@@ -2,7 +2,7 @@ LOGFILE="output.log"
 CONF="-b --configuration json://configuration_mc.json"
 
 
-o2-analysis-lf-lithium4findables $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
+o2-analysis-lf-lithium4findables $CONF|\
 
     # converters
     # o2-analysis-tracks-extra-converter $CONF|
@@ -10,15 +10,15 @@ o2-analysis-lf-lithium4findables $CONF --shm-segment-size 750000000000 --aod-mem
     # o2-analysis-bc-converter $CONF|
 
     # standard wagons
-    o2-analysis-timestamp $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
-    o2-analysis-event-selection $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|      
-    o2-analysis-track-propagation $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000 --aod-writer-json OutputDirector_mc.json > $LOGFILE
-    # o2-analysis-trackselection $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
-    # o2-analysis-multiplicity-table $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
-    # o2-analysis-pid-tpc-base $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
-    # o2-analysis-pid-tpc-full $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
-    # o2-analysis-pid-tof-base $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000|
-    # o2-analysis-pid-tof-full $CONF --shm-segment-size 750000000000 --aod-memory-rate-limit 50000000000 
+    o2-analysis-timestamp $CONF|\
+    o2-analysis-event-selection $CONF|\
+    o2-analysis-track-propagation $CONF|\
+    o2-analysis-trackselection $CONF|\
+    o2-analysis-multiplicity-table $CONF|\
+    o2-analysis-pid-tpc-base $CONF|\
+    o2-analysis-pid-tpc-full $CONF|\
+    o2-analysis-pid-tof-base $CONF|\
+    o2-analysis-pid-tof-full $CONF  > $LOGFILE
 
 
 # report the status of the workflow
