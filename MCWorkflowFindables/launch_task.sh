0dc546e3f696cd697d99a137b6931991ca312420
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2024-06-25 16:39:31 +0200

    .

---
+++ MCWorkflowFindables/launch_task.sh
@@ -0,0 +1,34 @@
+LOGFILE="output.log"
+CONF="-b --configuration json://configuration_mc.json"
+
+
+o2-analysis-lf-lithium4findables $CONF|\
+
+    # converters
+    # o2-analysis-tracks-extra-converter $CONF|
+    # o2-analysis-mc-converter $CONF|
+    # o2-analysis-bc-converter $CONF|
+
+    # standard wagons
+    o2-analysis-timestamp $CONF|\
+    o2-analysis-event-selection $CONF|\
+    o2-analysis-track-propagation $CONF|\
+    o2-analysis-trackselection $CONF|\
+    o2-analysis-multiplicity-table $CONF|\
+    o2-analysis-pid-tpc-base $CONF|\
+    o2-analysis-pid-tpc-full $CONF|\
+    o2-analysis-pid-tof-base $CONF|\
+    o2-analysis-pid-tof-full $CONF  > $LOGFILE
+
+
+# report the status of the workflow
+rc=$?
+if [ $rc -eq 0 ]; then
+    echo "Workflow finished successfully"
+else
+    echo "Error: Workflow failed with status $rc"
+    echo "Check the log file for more details: $LOGFILE"
+    exit $rc
+fi
+
+    
