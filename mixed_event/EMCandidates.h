dba705a099176c0086d81f17728107bd22cd2acf
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2024-12-20 07:35:28 +0100

    event mixing from fmazzasc

---
+++ mixed_event/EMCandidates.h
@@ -0,0 +1,179 @@
+#include <TTree.h>
+
+struct CollCandidate
+{
+    float fZVertex = -99., fCentralityFT0C;
+    int CollID = -1;
+    void setCollBranchAddress(TTree * tree) {
+        tree->SetBranchAddress("fZVertex", &fZVertex);
+        tree->SetBranchAddress("fCentralityFT0C", &fCentralityFT0C);
+    }
+};
+
+struct CollHadBracket
+{
+    int CollID, fHadStartIndex, fHadEndIndex;
+    void SetMin(int min) {
+        fHadStartIndex = min;
+    }
+    void SetMax(int max) {
+        fHadEndIndex = max;
+    }
+    int GetMin() {
+        return fHadStartIndex;
+    }
+    int GetMax() {
+        return fHadEndIndex;
+    }
+};
+
+
+struct He3Candidate
+{
+    float fPtHe3, fEtaHe3, fPhiHe3, fDCAxyHe3, fDCAzHe3, fSignalTPCHe3, fInnerParamTPCHe3, fMassTOFHe3;
+    unsigned int fItsClusterSizeHe3, fPIDtrkHe3;
+    unsigned char fNClsTPCHe3, fSharedClustersHe3;
+    float fNSigmaTPCHe3, fChi2TPCHe3;
+    float fZHe3, fCentralityFT0C;
+    int CollID = -1;
+    
+    void setHe3BranchAddress(TTree * tree) {
+        tree->SetBranchAddress("fPtHe3", &fPtHe3);
+        tree->SetBranchAddress("fEtaHe3", &fEtaHe3);
+        tree->SetBranchAddress("fPhiHe3", &fPhiHe3);
+        tree->SetBranchAddress("fDCAxyHe3", &fDCAxyHe3);
+        tree->SetBranchAddress("fDCAzHe3", &fDCAzHe3);
+        tree->SetBranchAddress("fSignalTPCHe3", &fSignalTPCHe3);
+        tree->SetBranchAddress("fInnerParamTPCHe3", &fInnerParamTPCHe3);
+        tree->SetBranchAddress("fMassTOFHe3", &fMassTOFHe3);
+        tree->SetBranchAddress("fNClsTPCHe3", &fNClsTPCHe3);
+        tree->SetBranchAddress("fItsClusterSizeHe3", &fItsClusterSizeHe3);
+        tree->SetBranchAddress("fPIDtrkHe3", &fPIDtrkHe3);
+        tree->SetBranchAddress("fSharedClustersHe3", &fSharedClustersHe3);
+        tree->SetBranchAddress("fNSigmaTPCHe3", &fNSigmaTPCHe3);
+        tree->SetBranchAddress("fChi2TPCHe3", &fChi2TPCHe3);
+    }
+
+};
+
+struct HadronCandidate
+{
+    float fPtHad, fEtaHad, fPhiHad, fDCAxyHad, fDCAzHad, fSignalTPCHad, fInnerParamTPCHad, fMassTOFHad;
+    unsigned int fItsClusterSizeHad, fPIDtrkHad;
+    unsigned char fSharedClustersHad;
+    float fNSigmaTPCHad, fChi2TPCHad;
+    float fZHad, fCentralityFT0C;
+    int CollID = -1;
+
+    void setHadronBranchAddress(TTree * tree) {
+        tree->SetBranchAddress("fPtHad", &fPtHad);
+        tree->SetBranchAddress("fEtaHad", &fEtaHad);
+        tree->SetBranchAddress("fPhiHad", &fPhiHad);
+        tree->SetBranchAddress("fDCAxyHad", &fDCAxyHad);
+        tree->SetBranchAddress("fDCAzHad", &fDCAzHad);
+        tree->SetBranchAddress("fSignalTPCHad", &fSignalTPCHad);
+        tree->SetBranchAddress("fInnerParamTPCHad", &fInnerParamTPCHad);
+        tree->SetBranchAddress("fMassTOFHad", &fMassTOFHad);
+        tree->SetBranchAddress("fItsClusterSizeHad", &fItsClusterSizeHad);
+        tree->SetBranchAddress("fPIDtrkHad", &fPIDtrkHad);
+        tree->SetBranchAddress("fSharedClustersHad", &fSharedClustersHad);
+        tree->SetBranchAddress("fNSigmaTPCHad", &fNSigmaTPCHad);
+        tree->SetBranchAddress("fChi2TPCHad", &fChi2TPCHad);
+    }
+};
+
+struct Li4Candidate
+{
+    float fPtHe3, fEtaHe3, fPhiHe3, fPtHad, fEtaHad, fPhiHad, fDCAxyHe3, fDCAzHe3, fDCAxyHad, fDCAzHad, fSignalTPCHe3, fInnerParamTPCHe3, fSignalTPCHad, fInnerParamTPCHad, fMassTOFHe3, fMassTOFHad;
+    unsigned int fItsClusterSizeHe3, fItsClusterSizeHad, fPIDtrkHe3, fPIDtrkHad;
+    unsigned char fNClsTPCHe3, fSharedClustersHe3, fSharedClustersHad;
+    float fNSigmaTPCHe3, fNSigmaTPCHad, fChi2TPCHe3, fChi2TPCHad;
+    float fZVertex, fCentralityFT0C;
+
+    void setHe3(He3Candidate & he3) {
+        fPtHe3 = he3.fPtHe3;
+        fEtaHe3 = he3.fEtaHe3;
+        fPhiHe3 = he3.fPhiHe3;
+        fDCAxyHe3 = he3.fDCAxyHe3;
+        fDCAzHe3 = he3.fDCAzHe3;
+        fSignalTPCHe3 = he3.fSignalTPCHe3;
+        fInnerParamTPCHe3 = he3.fInnerParamTPCHe3;
+        fMassTOFHe3 = he3.fMassTOFHe3;
+        fNClsTPCHe3 = he3.fNClsTPCHe3;
+        fItsClusterSizeHe3 = he3.fItsClusterSizeHe3;
+        fPIDtrkHe3 = he3.fPIDtrkHe3;
+        fSharedClustersHe3 = he3.fSharedClustersHe3;
+        fNSigmaTPCHe3 = he3.fNSigmaTPCHe3;
+        fChi2TPCHe3 = he3.fChi2TPCHe3;
+    }
+
+    void setHadron(HadronCandidate & had) {
+        fPtHad = had.fPtHad;
+        fEtaHad = had.fEtaHad;
+        fPhiHad = had.fPhiHad;
+        fDCAxyHad = had.fDCAxyHad;
+        fDCAzHad = had.fDCAzHad;
+        fSignalTPCHad = had.fSignalTPCHad;
+        fInnerParamTPCHad = had.fInnerParamTPCHad;
+        fMassTOFHad = had.fMassTOFHad;
+        fItsClusterSizeHad = had.fItsClusterSizeHad;
+        fPIDtrkHad = had.fPIDtrkHad;
+        fSharedClustersHad = had.fSharedClustersHad;
+        fNSigmaTPCHad = had.fNSigmaTPCHad;
+        fChi2TPCHad = had.fChi2TPCHad;
+    }
+
+    float calcInvMass() {
+        float pxHe3 = std::abs(fPtHe3) * TMath::Cos(fPhiHe3);
+        float pyHe3 = std::abs(fPtHe3) * TMath::Sin(fPhiHe3);
+        float pzHe3 = std::abs(fPtHe3) * TMath::SinH(fEtaHe3);
+        float pHe3 = TMath::Sqrt(pxHe3 * pxHe3 + pyHe3 * pyHe3 + pzHe3 * pzHe3);
+        float pxHad = std::abs(fPtHad) * TMath::Cos(fPhiHad);
+        float pyHad = std::abs(fPtHad) * TMath::Sin(fPhiHad);
+        float pzHad = std::abs(fPtHad) * TMath::SinH(fEtaHad);
+        float pHad = TMath::Sqrt(pxHad * pxHad + pyHad * pyHad + pzHad * pzHad);
+        float eHe3 = TMath::Sqrt(pHe3 * pHe3 + 2.8083916 * 2.8083916);
+        float eHad = TMath::Sqrt(pHad * pHad + 0.938272 * 0.938272);
+        float pTot = TMath::Sqrt((pxHe3 + pxHad) * (pxHe3 + pxHad) + (pyHe3 + pyHad) * (pyHe3 + pyHad) + (pzHe3 + pzHad) * (pzHe3 + pzHad));
+        float eTot = eHe3 + eHad;
+        return TMath::Sqrt(eTot * eTot - pTot * pTot);
+    }
+
+    float calcPt() {
+        float pxTot = std::abs(fPtHe3) * TMath::Cos(fPhiHe3) + std::abs(fPtHad) * TMath::Cos(fPhiHad);
+        float pyTot = std::abs(fPtHe3) * TMath::Sin(fPhiHe3) + std::abs(fPtHad) * TMath::Sin(fPhiHad);
+        return TMath::Sqrt(pxTot * pxTot + pyTot * pyTot);
+    }
+
+    void setLi4Branch(TTree * tree) {
+        tree->Branch("fPtHe3", &fPtHe3);
+        tree->Branch("fEtaHe3", &fEtaHe3);
+        tree->Branch("fPhiHe3", &fPhiHe3);
+        tree->Branch("fPtHad", &fPtHad);
+        tree->Branch("fEtaHad", &fEtaHad);
+        tree->Branch("fPhiHad", &fPhiHad);
+        tree->Branch("fDCAxyHe3", &fDCAxyHe3);
+        tree->Branch("fDCAzHe3", &fDCAzHe3);
+        tree->Branch("fDCAxyHad", &fDCAxyHad);
+        tree->Branch("fDCAzHad", &fDCAzHad);
+        tree->Branch("fSignalTPCHe3", &fSignalTPCHe3);
+        tree->Branch("fInnerParamTPCHe3", &fInnerParamTPCHe3);
+        tree->Branch("fSignalTPCHad", &fSignalTPCHad);
+        tree->Branch("fInnerParamTPCHad", &fInnerParamTPCHad);
+        tree->Branch("fMassTOFHe3", &fMassTOFHe3);
+        tree->Branch("fMassTOFHad", &fMassTOFHad);
+        tree->Branch("fItsClusterSizeHe3", &fItsClusterSizeHe3);
+        tree->Branch("fItsClusterSizeHad", &fItsClusterSizeHad);
+        tree->Branch("fPIDtrkHe3", &fPIDtrkHe3);
+        tree->Branch("fPIDtrkHad", &fPIDtrkHad);
+        tree->Branch("fSharedClustersHe3", &fSharedClustersHe3);
+        tree->Branch("fSharedClustersHad", &fSharedClustersHad);
+        tree->Branch("fNSigmaTPCHe3", &fNSigmaTPCHe3);
+        tree->Branch("fNSigmaTPCHad", &fNSigmaTPCHad);
+        tree->Branch("fChi2TPCHe3", &fChi2TPCHe3);
+        tree->Branch("fChi2TPCHad", &fChi2TPCHad);
+        tree->Branch("fZVertex", &fZVertex);
+        tree->Branch("fCentralityFT0C", &fCentralityFT0C);
+    }
+
+};
---
+++ mixed_event/processEM.C
@@ -0,0 +1,226 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <TTree.h>
+#include <TFile.h>
+#include <TKey.h>
+#include <TDirectory.h>
+#include <TList.h>
+#include <TString.h>
+#include <TRandom3.h>
+#include <TCanvas.h>
+#include <TH1D.h>
+#include "IndexTableUtils.h"
+#include "EMCandidates.h"
+
+void TreeMerging(const char *inputFileName, const char *treeName, TFile *outputFile)
+{
+
+    TFile *inputFile = TFile::Open(inputFileName, "READ");
+    TList *treeList = new TList();
+    TIter nextDir(inputFile->GetListOfKeys());
+    TKey *key;
+    while ((key = (TKey *)nextDir()))
+    {
+        std::cout << "Reading directory: " << key->GetName() << std::endl;
+        TObject *obj = key->ReadObj();
+
+        if (obj->InheritsFrom(TDirectory::Class()))
+        {
+            TDirectory *dir = (TDirectory *)obj;
+            TTree *tmpTree = (TTree *)dir->Get(treeName);
+            treeList->Add(tmpTree);
+        }
+        else
+        {
+            std::cerr << "Missing trees in directory: " << key->GetName() << std::endl;
+        }
+    }
+
+    outputFile->cd();
+    TTree *tree = TTree::MergeTrees(treeList);
+    tree->Write();
+    inputFile->Close();
+}
+
+void processEM(bool doMerge = false)
+{
+    TH1D* hHe3BeforeEMAll = new TH1D("hHe3BeforeEMAll", "; p_{T} (GeV/c); Entries", 200, -10, 0);
+    TH1D* hHe3BeforeEM = new TH1D("hHe3BeforeEM", "; p_{T} (GeV/c); Entries", 200, -10, 0);
+    TH1D* hHe3AfterEM = new TH1D("hHe3AfterEM", "; p_{T} (GeV/c); Entries", 200, -10, 0);
+    TH1D* hInvMassBeforeEM = new TH1D("hInvMassBeforeEM", "; Inv Mass (GeV/c^{2}); Entries", 200, 3.743, 4.143);
+    TH1D* hInvMassAfterEM = new TH1D("hInvMassAfterEM", "; Inv Mass (GeV/c^{2}); Entries", 200, 3.743, 4.143);
+
+    gRandom->SetSeed(1995);
+    int mEMDepth = 50;
+
+    if (doMerge)
+    {
+        std::string inputFileName = "/data/galucia/lithium_local/same/LHC23_PbPb_pass4_long_same.root";
+        std::string treeNameCands = "O2he3hadtable";
+        std::string treeNameColls = "O2he3hadmult";
+        TFile * inputCandsFile = TFile::Open("inputCands.root", "RECREATE");
+        TFile * inputCollsFile = TFile::Open("inputColls.root", "RECREATE");
+        TreeMerging(inputFileName.c_str(), treeNameCands.c_str(), inputCandsFile);
+        TreeMerging(inputFileName.c_str(), treeNameColls.c_str(), inputCollsFile);
+        inputCandsFile->Close();
+        inputCollsFile->Close();
+    }
+
+    TFile *inputCandsFile = TFile::Open("inputCands.root");
+    TTree *inputCandsTree = (TTree *)inputCandsFile->Get("O2he3hadtable");
+    TFile *inputCollsFile = TFile::Open("inputColls.root");
+    TTree *inputCollsTree = (TTree *)inputCollsFile->Get("O2he3hadmult");
+
+    IndexTableUtils mUtils;
+    std::vector<std::vector<CollHadBracket>> mCollBrackets;
+    mCollBrackets.resize(mUtils.mZetaBins * mUtils.mMultBins + 1);
+
+    CollCandidate collCand;
+    CollHadBracket collBracket;
+
+    He3Candidate he3Cand;
+    HadronCandidate hadCand;
+    Li4Candidate li4Cand;
+
+    collCand.setCollBranchAddress(inputCollsTree);
+    he3Cand.setHe3BranchAddress(inputCandsTree);
+    hadCand.setHadronBranchAddress(inputCandsTree);
+
+    std::vector<He3Candidate> mHe3Cands;
+    std::vector<HadronCandidate> mHadCands;
+    std::vector<CollCandidate> mCollCands;
+
+    collBracket.SetMin(-1);
+    collBracket.SetMax(-1);
+    CollCandidate collCandPrev;
+
+    for (int iEntry = 0; iEntry < inputCollsTree->GetEntries(); iEntry++)
+    {
+        inputCollsTree->GetEntry(iEntry);
+        inputCandsTree->GetEntry(iEntry);
+        hadCand.fZHad = collCand.fZVertex;
+        hadCand.fCentralityFT0C = collCand.fCentralityFT0C;
+        mHadCands.push_back(hadCand);
+
+        li4Cand.setHadron(hadCand);
+        li4Cand.setHe3(he3Cand);
+        if (he3Cand.fPtHe3 < 0.) {
+            hInvMassBeforeEM->Fill(li4Cand.calcInvMass());
+        }
+        hHe3BeforeEMAll->Fill(he3Cand.fPtHe3);
+
+        if (abs(collCandPrev.fZVertex - collCand.fZVertex) < 1e-5)
+        {
+            collBracket.SetMax(mHadCands.size() - 1);
+            continue;
+        }
+
+        if (collBracket.GetMin() != -1)
+        {
+            int iBin = mUtils.getBinIndex(collCandPrev.fZVertex, collCandPrev.fCentralityFT0C);
+            collBracket.CollID = iEntry - 1;
+            mCollBrackets[iBin].push_back(collBracket);
+        }
+
+        // a new collision has been found, dumping collision and he3 candidates
+        
+        mHe3Cands.push_back(he3Cand); 
+        mCollCands.push_back(collCand);
+        hHe3BeforeEM->Fill(he3Cand.fPtHe3);
+
+        collBracket.SetMin(mHadCands.size() - 1);
+        collBracket.SetMax(mHadCands.size() - 1);
+        collCandPrev = collCand;
+    }
+
+    std::cout << "--------------------------------" << std::endl;
+    std::cout << "Size of Hadron Candidates to be mixed: " << mHadCands.size() << std::endl;
+    std::cout << "Size of He3 Candidates to be mixed: " << mHe3Cands.size() << std::endl;
+    std::cout << "Size of Coll Candidates: " << mCollCands.size() << std::endl;
+    std::cout << "--------------------------------" << std::endl;
+
+    Li4Candidate li4CandME;
+    auto outputFile = TFile::Open("/data/galucia/lithium_local/mixing/LHC23_PbPb_pass4_long_mixing_nocut.root", "RECREATE");
+    auto outputTree = new TTree("MixedTree", "MixedTree");
+    // flash the Mixed event structure in the output tree
+    //outputTree->Branch("O2he3hadtable", &li4CandME);
+    li4CandME.setLi4Branch(outputTree);
+
+    std::cout << "--------------------------------" << std::endl;
+    for (int collIDHe3 = 0; collIDHe3 < mHe3Cands.size(); collIDHe3++)
+    {
+
+        // print 1% progress
+        if (collIDHe3 % (mHe3Cands.size() / 100) == 0)
+        {
+            std::cout << "Processing entry: " << collIDHe3 << " / " << mHe3Cands.size() << ", " << 100 * collIDHe3 / mHe3Cands.size() << "%\r" << std::endl;
+        }
+
+        auto &he3Cand = mHe3Cands[collIDHe3];
+        auto &collCand = mCollCands[collIDHe3];
+        li4CandME.setHe3(he3Cand);
+        int iBin = mUtils.getBinIndex(collCand.fZVertex, collCand.fCentralityFT0C);
+
+        for (int iDepth = 0; iDepth < mEMDepth; iDepth++)
+        {
+
+            if (mCollBrackets[iBin].size() == 0 || iDepth >= mCollBrackets[iBin].size())
+            {
+                break;
+            }
+
+            int iCollEM;
+            mCollBrackets[iBin].size() < mEMDepth ? iCollEM = iDepth : iCollEM = gRandom->Integer(mCollBrackets[iBin].size());
+            int collIDHad = mCollBrackets[iBin][iCollEM].CollID;
+            if (collIDHad == collIDHe3)
+            {
+                continue;
+            }
+            for (int iHad = mCollBrackets[iBin][iCollEM].GetMin(); iHad <= mCollBrackets[iBin][iCollEM].GetMax(); iHad++)
+            {
+                auto &hadCand = mHadCands[iHad];
+                li4CandME.setHadron(hadCand);
+                li4CandME.fZVertex = collCand.fZVertex;
+                li4CandME.fCentralityFT0C = collCand.fCentralityFT0C;
+                // std::cout << "inv mass: " << li4CandME.calcInvMass() << std::endl;
+                //if (li4CandME.calcInvMass() < 4.15314 && li4CandME.fPtHe3 * li4CandME.fPtHad > 0 && li4CandME.calcPt() > 2){
+                //if (li4CandME.calcInvMass() < 4.15314 && li4CandME.calcPt() > 2){ // like-sign and unlike-sign
+                if (li4CandME.calcPt() > 2){ // like-sign and unlike-sign
+                    if (li4CandME.fPtHe3 < 0) {
+                        hInvMassAfterEM->Fill(li4CandME.calcInvMass());
+                    }
+                    hHe3AfterEM->Fill(he3Cand.fPtHe3);
+                    outputTree->Fill();
+                }
+            }
+        }
+    }
+    std::cout << std::endl << "--------------------------------" << std::endl;
+
+    hHe3BeforeEMAll->Write();
+    hHe3BeforeEM->Write();
+    hHe3AfterEM->Write();
+
+    hInvMassBeforeEM->Write();
+    hInvMassAfterEM->Write();
+
+    // plot in the same canvas
+    hHe3BeforeEMAll->SetLineColor(kBlack);
+    hHe3BeforeEM->SetLineColor(kRed);
+    hHe3AfterEM->SetLineColor(kBlue);
+    TCanvas *c = new TCanvas("c", "c", 800, 600);
+    hHe3BeforeEMAll->DrawNormalized();
+    hHe3BeforeEM->DrawNormalized("SAME");
+    hHe3AfterEM->DrawNormalized("SAME");
+    c->Write();
+
+    hInvMassBeforeEM->SetLineColor(kRed);
+    hInvMassAfterEM->SetLineColor(kBlue);
+    TCanvas *c2 = new TCanvas("c2", "c2", 800, 600);
+    hInvMassBeforeEM->DrawNormalized();
+    hInvMassAfterEM->DrawNormalized("SAME");
+    c2->Write();
+
+    outputFile->Close();
+}
