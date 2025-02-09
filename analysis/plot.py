2b9651fc252110d0f09d46bc4cf41e0e1037bbd8
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2025-02-09 22:24:50 +0100

    updated plotter

--- analysis/plot.py
+++ analysis/plot.py
@@ -2,17 +2,18 @@ import yaml
 
 import sys
 sys.path.append('..')
-from framework.src.plotter import Plotter
+from torchic import Plotter
 
 if __name__ == '__main__':
 
     #input_file = '/home/galucia/antiLithium4/analysis/config/cfgPlot.yml'
     #input_file = '/home/galucia/antiLithium4/analysis/config/cfgPlotPresentation.yml'
-    #input_file = '/home/galucia/antiLithium4/analysis/figures/PAG_07112024/cfgPAG_07112024.yml'
+    ##input_file = '/home/galucia/antiLithium4/analysis/figures/PAG_07112024/cfgPAG_07112024.yml'
     #input_file = '/home/galucia/antiLithium4/analysis/figures/03-01-2025-Non-Normalised/cfgNonNormalised.yml'
     #input_file = '/home/galucia/antiLithium4/analysis/figures/03-01-2025-Normalised-Grid/cfgNormalised.yml'
     #input_file = '/home/galucia/antiLithium4/analysis/figures/03-01-2025-Corrected-InvMass/cfgCorrected.yml'
     input_file = '/home/galucia/antiLithium4/analysis/figures/03-01-2025-Non-Corrected-InvMass/cfgNonCorrected.yml'
+    input_file = '/Users/glucia/Projects/ALICE/antiLithium4/analysis/figures/LHC24PbPb/cfg.yml'
 
     with open(input_file, 'r') as f:
         config = yaml.safe_load(f)
@@ -21,37 +22,38 @@ if __name__ == '__main__':
 
     for plot in config['plots']:
 
-        plotter.createCanvas(plot['axisSpecs'], **plot['canvas'])
-        plotter.createMultiGraph(plot['axisSpecs'])
-        if plot['legend']['bool']:  
-            position = [plot['legend']['xmin'], plot['legend']['ymin'], plot['legend']['xmax'], plot['legend']['ymax']]
-            plotter.createLegend(position, **plot['legend']['kwargs'])
+        plotter.create_canvas(plot['axisSpecs'], **plot['canvas'])
+        plotter.create_multigraph(plot['axisSpecs'])
+        if 'legends' in plot.keys():
+            for legend in plot['legends']:
+                position = [legend['xmin'], legend['ymin'], legend['xmax'], legend['ymax']]
+                plotter.create_legend(position, **legend['kwargs'])
 
         if 'graphs' in plot:
             for graph in plot['graphs']:
-                plotter.addGraph(graph['inPath'], graph['graphName'], graph['graphLabel'], **graph['kwargs'])
+                plotter.add_graph(graph['inPath'], graph['graphName'], graph['graphLabel'], **graph['kwargs'])
 
         if 'hists' in plot.keys():
             for hist in plot['hists']:
-                plotter.addHist(hist['inPath'], hist['histName'], hist['histLabel'], **hist['kwargs'])
+                plotter.add_hist(hist['inPath'], hist['histName'], hist['histLabel'], **hist['kwargs'])
 
         if 'ROIs' in plot.keys():
             for roi in plot['ROIs']:
-                plotter.addROI(roi['lineSpecs'], roi['boxSpecs'], **roi['kwargs'])
+                plotter.add_ROI(roi['lineSpecs'], roi['boxSpecs'], **roi['kwargs'])
 
         if 'lines' in plot.keys():
             for line in plot['lines']:
-                plotter.addLine(line['lineSpecs'], **line['kwargs'])
+                plotter.add_line(line['lineSpecs'], **line['kwargs'])
 
         if 'funcs' in plot:
             for func in plot['funcs']:
-                plotter.addFunc(func['inPath'], func['funcName'], func['funcLabel'], **func['kwargs'])
+                plotter.add_func(func['inPath'], func['funcName'], func['funcLabel'], **func['kwargs'])
         
         if 'multigraph' in plot:
-            plotter.drawMultiGraph(**plot['multigraph']['kwargs'])
-        if plot['legend']['bool']:  
-            plotter.drawLegend(**plot['legend']['kwargs'])
+            plotter.draw_multigraph(**plot['multigraph']['kwargs'])
+        
+        plotter.draw_legend()
 
         plotter.save(plot['outPDF'])
     
-    plotter.outFile.Close()
+    plotter.outfile.Close()
