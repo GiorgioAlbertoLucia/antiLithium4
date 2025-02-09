ccf923b801dbe8a4d6832942d312f6d8d922a230
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2025-02-09 22:24:42 +0100

    updated pid in tracking correction

--- analysis/he3_pid_tracking_correction.py
+++ analysis/he3_pid_tracking_correction.py
@@ -11,7 +11,7 @@ def parametrise_resolution(h2_res, xmin, xmax, ymin, ymax, output_file: TDirecto
     x = RooRealVar('x', 'x', ymin, ymax)
     #roofitter = Roofitter(x, ['exp_mod_gaus', 'crystal_ball'])
     roofitter = Roofitter(x, ['exp_mod_gaus'])
-    roofitter.init_param('exp_mod_gaus_0_mean', 0.0, -0.5, 0.5)
+    roofitter.init_param('exp_mod_gaus_0_mean', 0.1, 0.0, 0.5)
     roofitter.init_param('exp_mod_gaus_0_sigma', 0.01, 0.0, 1.0)
     roofitter.init_param('exp_mod_gaus_0_tau', 0.1, -10.0, 10.0)
     
@@ -61,34 +61,37 @@ def parametrise_resolution(h2_res, xmin, xmax, ymin, ymax, output_file: TDirecto
 
 if __name__ == '__main__':
 
-    dataset = Dataset.from_root('/data/galucia/lithium_local/MC/LHC24b2c_nucleispectra_injectedhe3.root', 
-                                tree_name='O2nucleitable',
-                                folder_name='DF*')
-
+    dataset = Dataset.from_root('/Users/glucia/Projects/ALICE/data/lithium/MC/LHC25a4.root', 
+                                    tree_name='O2he3hadtable',
+                                    folder_name='DF*')
+    dataset = dataset.concat(Dataset.from_root('/Users/glucia/Projects/ALICE/data/lithium/MC/LHC25a4.root', 
+                                    tree_name='O2he3hadtablemc',
+                                    folder_name='DF*'), axis=1)
     print(f'{dataset.columns=}')
 
     # select he3
-    dataset.query('abs(fPDGcode) == 1000020030', inplace=True)
-    readPidTracking = lambda x: (x >> 12) & 0x1F
-    readPidTracking = np.vectorize(readPidTracking)
-    dataset['fPidTracking'] = readPidTracking(dataset['fFlags'])
-    dataset.add_subset('H3', dataset['fPidTracking'] == 6)
-    dataset.add_subset('He4', dataset['fPidTracking'] == 8)
-    dataset.loc[dataset['fPDGcode'] < 0, 'fPt'] = - dataset['fPt']
+    #dataset.query('abs(fPDGcode) == 1000020030', inplace=True)
+    #readPidTracking = lambda x: (x >> 12) & 0x1F
+    #readPidTracking = np.vectorize(readPidTracking)
+    #dataset['fPidTracking'] = readPidTracking(dataset['fFlags'])
+    dataset.add_subset('H3', dataset['fPIDtrkHe3'] == 6)
+    dataset.add_subset('He4', dataset['fPIDtrkHe3'] == 8)
+    #dataset.loc[dataset['fPDGcode'] < 0, 'fPt'] = - dataset['fPt']
 
-    dataset['fPt'] = dataset['fPt'] * 2.0
-    dataset['fPtDiff'] = dataset['fPt'] - dataset['fgPt']
-    dataset['fPtRes'] = dataset['fPtDiff'] / dataset['fPt']
+    dataset['fPtHe3'] = abs(dataset['fPtHe3'])
+    dataset['fPtDiff'] = dataset['fPtHe3'] - dataset['fPtMCHe3']
+    dataset['fPtRes'] = dataset['fPtDiff'] / dataset['fPtHe3']
 
-    output_file_path = '/home/galucia/antiLithium4/analysis/output/MC/he3PidTrackingCorrection.root'
-    axis_spec_pt = AxisSpec(100, 0., 5., 'fPt', ';#it{p}_{T}^{rec} (GeV/#it{c})')
-    axis_spec_gpt = AxisSpec(100, 0., 5., 'fgPt', ';#it{p}_{T}^{gen} (GeV/#it{c})')
+    #output_file_path = '/home/galucia/antiLithium4/analysis/output/MC/he3PidTrackingCorrection.root'
+    output_file_path = '/Users/glucia/Projects/ALICE/antiLithium4/analysis/output/MC/he3_pid_trk_correction.root'
+    axis_spec_pt = AxisSpec(100, 0., 5., 'fPtHe3', ';#it{p}_{T}^{rec} (GeV/#it{c})')
+    axis_spec_gpt = AxisSpec(100, 0., 5., 'fPtMCHe3', ';#it{p}_{T}^{gen} (GeV/#it{c})')
 
-    h_pt = dataset.build_hist('fPt', axis_spec_pt)
-    h_gpt = dataset.build_hist('fgPt', axis_spec_gpt)
-    h2_res = dataset.build_hist('fPt', 'fPtRes', axis_spec_pt, AxisSpec(100, -0.5, 0.5, 'fPtRes', ';#it{p}_{T}^{rec} (GeV/#it{c});(#it{p}_{T}^{rec} - #it{p}_{T}^{gen}) / #it{p}_{T}^{rec}'))
-    h2_resh3 = dataset.build_hist('fPt', 'fPtRes', axis_spec_pt, AxisSpec(100, -0.5, 0.5, 'fPtResH3', ';#it{p}_{T}^{rec} (GeV/#it{c});(#it{p}_{T}^{rec} - #it{p}_{T}^{gen}) / #it{p}_{T}^{rec}'), subset='H3')
-    h2_reshe4 = dataset.build_hist('fPt', 'fPtRes', axis_spec_pt, AxisSpec(100, -0.5, 0.5, 'fPtResHe4', ';#it{p}_{T}^{rec} (GeV/#it{c});(#it{p}_{T}^{rec} - #it{p}_{T}^{gen}) / #it{p}_{T}^{rec}'), subset='He4')
+    h_pt = dataset.build_th1('fPtHe3', axis_spec_pt)
+    h_gpt = dataset.build_th1('fPtMCHe3', axis_spec_gpt)
+    h2_res = dataset.build_th2('fPtHe3', 'fPtRes', axis_spec_pt, AxisSpec(100, -0.5, 0.5, 'fPtRes', ';#it{p}_{T}^{rec} (GeV/#it{c});(#it{p}_{T}^{rec} - #it{p}_{T}^{gen}) / #it{p}_{T}^{rec}'))
+    h2_resh3 = dataset.build_th2('fPtHe3', 'fPtRes', axis_spec_pt, AxisSpec(100, -0.5, 0.5, 'fPtResH3', ';#it{p}_{T}^{rec} (GeV/#it{c});(#it{p}_{T}^{rec} - #it{p}_{T}^{gen}) / #it{p}_{T}^{rec}'), subset='H3')
+    h2_reshe4 = dataset.build_th2('fPtHe3', 'fPtRes', axis_spec_pt, AxisSpec(100, -0.5, 0.5, 'fPtResHe4', ';#it{p}_{T}^{rec} (GeV/#it{c});(#it{p}_{T}^{rec} - #it{p}_{T}^{gen}) / #it{p}_{T}^{rec}'), subset='He4')
 
     output_file = TFile.Open(output_file_path, 'recreate')
     h_pt.Write()
@@ -97,16 +100,17 @@ if __name__ == '__main__':
     h2_resh3.Write()
     h2_reshe4.Write()
 
-    fit_res_results = parametrise_resolution(h2_resh3, 1.4, 2.35, -0.5, 0.5, output_file)
+    fit_res_results = parametrise_resolution(h2_resh3, 1.55, 2.2, -0.5, 0.5, output_file)
 
     # pol1 correction
-    dataset.loc[dataset['fPidTracking'] == 6, 'fPt'] = dataset['fPt'] - dataset['fPt']*(fit_res_results[0] + fit_res_results[1] * dataset['fPt'])
+    dataset.loc[dataset['fPIDtrkHe3'] == 6, 'fPtHe3'] = dataset['fPtHe3'] - dataset['fPtHe3']*(fit_res_results[0] + fit_res_results[1] * dataset['fPtHe3'])
     # pol2 correction
     # dataset.loc[dataset['fPidTracking'] == 6, 'fPt'] = dataset['fPt'] - dataset['fPt']*(fit_res_results[0] + fit_res_results[1] * dataset['fPt'] + fit_res_results[2] * dataset['fPt']**2)
-    dataset['fPtDiff'] = dataset['fPt'] - dataset['fgPt']
-    dataset['fPtRes'] = dataset['fPtDiff'] / dataset['fPt']
-    h2_res_corrected = dataset.build_hist('fPt', 'fPtRes', axis_spec_pt, AxisSpec(100, -0.5, 0.5, 'fPtResCorrected', ';#it{p}_{T}^{rec} (GeV/#it{c});(#it{p}_{T}^{rec} - #it{p}_{T}^{gen}) / #it{p}_{T}^{rec}'))
-    h2_resh3_corrected = dataset.build_hist('fPt', 'fPtRes', axis_spec_pt, AxisSpec(100, -0.5, 0.5, 'fPtResH3Corrected', ';#it{p}_{T}^{rec} (GeV/#it{c});(#it{p}_{T}^{rec} - #it{p}_{T}^{gen}) / #it{p}_{T}^{rec}'), subset='H3')
+    
+    dataset['fPtDiff'] = dataset['fPtHe3'] - dataset['fPtMCHe3']
+    dataset['fPtRes'] = dataset['fPtDiff'] / dataset['fPtHe3']
+    h2_res_corrected = dataset.build_th2('fPtHe3', 'fPtRes', axis_spec_pt, AxisSpec(100, -0.5, 0.5, 'fPtResCorrected', ';#it{p}_{T}^{rec} (GeV/#it{c});(#it{p}_{T}^{rec} - #it{p}_{T}^{gen}) / #it{p}_{T}^{rec}'))
+    h2_resh3_corrected = dataset.build_th2('fPtHe3', 'fPtRes', axis_spec_pt, AxisSpec(100, -0.5, 0.5, 'fPtResH3Corrected', ';#it{p}_{T}^{rec} (GeV/#it{c});(#it{p}_{T}^{rec} - #it{p}_{T}^{gen}) / #it{p}_{T}^{rec}'), subset='H3')
 
     output_file.cd()
     h2_res_corrected.Write()
