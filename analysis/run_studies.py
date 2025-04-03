4ea5e17073a65b7fb761ea94f83e76303f56b76d
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2025-04-03 14:19:58 +0200

    removed unnecessary studies

--- analysis/run_studies.py
+++ analysis/run_studies.py
@@ -6,80 +6,15 @@ import numpy as np
 import argparse
 import yaml
 
-from ROOT import TF1, TFile
+from ROOT import TFile
 
-from studies.studies import StandaloneStudy
 from studies.correlationStudies import CorrelationStudy
 from studies.invMassStudies import InvariantMassStudy
-from studies.clusterStudies import ClusterSizeParamStudy
-from studies.betheBlochStudies import BetheBlochStudy
-from studies.comparisonStudies import ComparisonStudy
-from studies.TOFselectionStudies import TOFselectionStudy
 
-from torchic import Dataset, AxisSpec, HistLoadInfo
-from torchic.physics.calibration import py_BetheBloch
-from torchic.core.histogram import load_hist, project_hist
+from torchic import HistLoadInfo
+from torchic.core.histogram import load_hist
 from torchic.utils.terminal_colors import TerminalColors as tc
 
-import sys
-sys.path.append('..')
-from utils.particles import ParticleMasses
-
-def run_cluster_size_param_study(config, outputFile:TFile):
-    '''
-        Run the cluster size param study
-    '''
-
-    print(tc.GREEN+'[INFO]: '+tc.RESET+'Running the cluster size param study.')
-
-    bb_config = '/Users/glucia/Projects/ALICE/antiLithium4/analysis/config/cfg_BetheBloch.yml'
-    h2_cluster_info = HistLoadInfo('/Users/glucia/Projects/ALICE/antiLithium4/analysis/output/LHC24/data_visual_selectionsHe3.root',
-                                   'ClusterSize/ClusterSizevsPPr')
-    study = ClusterSizeParamStudy(config, outputFile, bb_config, h2_cluster_info)
-    study.rebinx(2)
-    study.draw('OLD_PARAMETERS_h2_cBB_Pr')    # fit the Bethe Bloch curve with the old parameters
-    study.fit()
-
-def run_tpc_calibration_study(config, outputFile:TFile):
-    '''
-        Run the TPC calibration study
-    '''
-
-    print(tc.GREEN+'[INFO]: '+tc.RESET+'Running the TPC calibration study.')
-
-    cfg = yaml.safe_load(open(config, 'r'))
-    #sameEventLoad = HistLoadInfo(cfg['SEFileTPCcalibration'],
-    #                             'TPC/dEdXvsBetaGammaHe3')
-    sameEventLoad = HistLoadInfo(cfg['SEFileTPCcalibration'],
-                                     'TPC/dEdXvsBetaGammaHad')
-
-    study = BetheBlochStudy(config, outputFile, sameEventLoad)
-    #study.rebinx(3)
-    #study.fit_BetheBloch(xMin=0.24, xMax=2.5, yMin=0.0, yMax=3000.0)
-    study.fit_BetheBloch(xMin=0.3, xMax=3.5, yMin=0.0, yMax=700.0)
-
-    axis_spec_p = AxisSpec(100, -5., 5, 'h2_nSigma', ';#it{p}_{TPC} (GeV/#it{c}); n#sigma_{TPC}')
-    axis_spec_bg = AxisSpec(40, 0., 4, 'h2_dEdxBetaGamma', ';#beta#gamma; Expected dE/dx_{TPC}')
-    axis_spec_nsigma_tpc = AxisSpec(100, -5., 5, 'h2_nSigmaTPC', ';#it{p}_{TPC} (GeV/#it{c}); n#sigma_{TPC}')
-    axis_spec_dEdx = AxisSpec(70, 0., 700., 'h2_dEdxBetaGamma', ';#beta#gamma; Expected dE/dx_{TPC}')
-
-    #dataset = Dataset.from_root(cfg['InputFileTPCcalibration'], tree_name='O2he3hadtable', folder_name='DF*', column_names=['fInnerParamTPCHe3', 'fSignalTPCHe3'])
-    #dataset['fInnerParamTPC'] = dataset['fInnerParamTPCHe3'] * 2.
-    #dataset['fTPCsignal'] = dataset['fSignalTPCHe3']
-    #mass = ParticleMasses['He3']
-    dataset = Dataset.from_root(cfg['InputFileTPCcalibration'], tree_name='O2he3hadtable', folder_name='DF*', columns=['fInnerParamTPCHad', 'fSignalTPCHad'])
-    dataset['fInnerParamTPC'] = dataset['fInnerParamTPCHad']
-    dataset['fTPCsignal'] = dataset['fSignalTPCHad']
-    mass = ParticleMasses['Pr']
-    dataset['fBetaGamma'] = dataset['fInnerParamTPC'] / mass
-    dataset['fExpSignalTPC'] = py_BetheBloch(dataset['fBetaGamma'], *study.BetheBlochParams.values())
-    dataset['fNSigmaTPC'] = (dataset['fTPCsignal'] - dataset['fExpSignalTPC']) / (0.09*dataset['fExpSignalTPC'])
-    h2_bg = dataset.build_hist('fBetaGamma', 'fExpSignalTPC', axis_spec_bg, axis_spec_dEdx)
-    h2_nsigma_tpc = dataset.build_hist('fInnerParamTPC', 'fNSigmaTPC', axis_spec_p, axis_spec_nsigma_tpc)
-
-    study.dir.cd()
-    h2_bg.Write()
-    h2_nsigma_tpc.Write()
 
 # temporary
 def add_hist(hist1, hist2):
@@ -132,7 +67,7 @@ def run_correlation_study(config, outputFile:TFile) -> float:
         study.save('_prerebin')
         #bin_edges = np.array([0.0, 0.1, 0.15, 0.2, 0.25, 0.3, 0.325, 0.35, 0.375, 0.4, 0.425, 0.45, 0.475, 0.5, 0.525, 0.55, 0.575, 0.6, 0.625, 0.65, 0.675, 0.7, 0.725, 0.75, 0.775, 0.8, 0.825, 0.85, 0.875, 0.9, 0.925, 0.95, 0.975, 1.0, 1.025, 1.05, 1.075, 1.1, 1.125, 1.15, 1.175, 1.2, 1.225, 1.25, 1.275, 1.3, 1.325, 1.35, 1.375, 1.4, 1.425, 1.45, 1.475, 1.5, 1.525, 1.55, 1.575, 1.6, 1.625, 1.65, 1.675, 1.7, 1.725, 1.75, 1.775, 1.8, 1.825, 1.85, 1.875, 1.9, 1.925, 1.95, 1.975, 2.0], dtype=np.float32)
         #bin_edges = np.array([0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 1.05, 1.1, 1.15, 1.2, 1.25, 1.3, 1.35, 1.4, 1.45, 1.5, 1.55, 1.6, 1.65, 1.7, 1.75, 1.8, 1.85, 1.9, 1.95, 2.0], dtype=np.float32)
-        bin_edges = np.linspace(0.0, 0.8, 40)
+        bin_edges = np.linspace(0.0, 0.8, 61)
         study.custom_binning(bin_edges)
         #study.rebin(5)
         #study.self_normalize()
@@ -172,51 +107,6 @@ def run_correlation_study(config, outputFile:TFile) -> float:
     print(tc.GREEN+'[INFO]: '+tc.RESET+'----------------------------------------------------------------')
 
     return norm_factor
-
-def run_close_pair_rejection(config, outputFile:TFile):
-
-    print(tc.GREEN+'[INFO]: '+tc.RESET+'Running the close pair rejection study.')   
-    cfg = yaml.safe_load(open(config, 'r'))
-
-    hDeltaEtaDeltaPhi = load_hist(HistLoadInfo(cfg['SEFileDeltaEtaDeltaPhi'], 'Correlations/DeltaEtaDeltaPhi'))
-    hDeltaEta = project_hist(hDeltaEtaDeltaPhi, min_int=-0.012, max_int=0.012, name='DeltaEta', var_to_project='X')
-    hDeltaPhi = project_hist(hDeltaEtaDeltaPhi, min_int=-0.024, max_int=0.012, name='DeltaPhi', var_to_project='Y')
-
-    fit_func = TF1('fit_func', '[0] * (1 - [3]*exp(-0.5*((x - [1])*(x - [1])/([2]*[2])) ))', -0.1, 0.1)
-    fit_func.SetParameter(0, 650)
-    fit_func.SetParameter(1, 0)
-    fit_func.SetParameter(2, 0.01)
-    fit_func.SetParLimits(2, 0.001, 0.1)
-    fit_func.SetParameter(3, 0.3)
-
-    hDeltaEta.Fit(fit_func, 'RML+')
-    hDeltaPhi.Fit(fit_func, 'RML+')
-
-    StandaloneStudy.cd()
-    cpr_dir = outputFile.mkdir('ClosePairRejection')
-    cpr_dir.cd()
-    hDeltaEta.Write()
-    hDeltaPhi.Write()
-
-def run_comparison_study(config, outputFile:TFile, scaling_factor=1.0):
-    '''
-        Run the comparison study
-    '''
-
-    print(tc.GREEN+'[INFO]: '+tc.RESET+'Running the comparison study.')
-
-    cfg = yaml.safe_load(open(config, 'r'))
-    study = ComparisonStudy(config, outputFile)
-    print(scaling_factor)
-
-    for var in cfg['comparisonVariables']:
-        sameEventLoad = HistLoadInfo(cfg['SEFileComparison'], var)
-        mixedEventLoad = HistLoadInfo(cfg['MEFileComparison'], var)
-        study.load_same_event(sameEventLoad)
-        study.load_mixed_event(mixedEventLoad)  
-        #study.self_normalize()  
-        #study.scale_mixing(scaling_factor)
-        study.save()
 
 def run_invariant_mass_study(config, outputFile:TFile):
     '''
@@ -274,19 +164,6 @@ def run_invariant_mass_study(config, outputFile:TFile):
     
     print(tc.GREEN+'[INFO]: '+tc.RESET+'----------------------------------------------------------------')
 
-def run_TOF_selection_study(config, outputFile:TFile):
-
-    print(tc.GREEN+'[INFO]: '+tc.RESET+'Running the TOF selection study.')
-
-    prLoad = HistLoadInfo('/Users/glucia/Projects/ALICE/antiLithium4/analysis/output/LHC24/event_mixing_visual_selectionsHe3.root',
-                         'TOF/TOFmassvsPtPr')
-    study = TOFselectionStudy(config,  outputFile,  prLoad)
-    study.rebinx(2)
-    study.fit_slices()
-    study.fit_resolution()
-    study.fit_mass()
-    study.save()
-
 def main():
 
     parser = argparse.ArgumentParser(description='Configure the parameters of the script.')
@@ -296,16 +173,6 @@ def main():
                         help='Run the correlation study.', default=False, action='store_true')
     parser.add_argument('--invMass', dest='run_invMass',
                         help='Run the invariant mass study.', default=False, action='store_true')
-    parser.add_argument('--clusterSize', dest='run_clusterSize',
-                        help='Run the cluster size param study.', default=False, action='store_true')
-    parser.add_argument('--comparison', dest='run_comparison',
-                        help='Run the comparison study.', default=False, action='store_true')
-    parser.add_argument('--TOF', dest='run_TOF',
-                        help='Run the TOF selection study.', default=False, action='store_true')
-    parser.add_argument('--TPCcalibration', dest='run_TPCcalibration',
-                        help='Run the TPC calibration study.', default=False, action='store_true')
-    parser.add_argument('--closePairRejection', dest='run_closePairRejection',
-                        help='Run the close pair rejection study.', default=False, action='store_true')
     args = parser.parse_args()
 
     #config = '/Users/glucia/Projects/ALICE/antiLithium4/analysis/config/cfg_studies.yml'
@@ -316,11 +183,6 @@ def main():
 
     if args.run_all or args.run_correlation:        norm_factor = run_correlation_study(config, outputFile)
     if args.run_all or args.run_invMass:            run_invariant_mass_study(config, outputFile)
-    if args.run_all or args.run_clusterSize:        run_cluster_size_param_study(config, outputFile)
-    if args.run_all or args.run_TOF:                run_TOF_selection_study(config, outputFile)
-    if args.run_all or args.run_TPCcalibration:     run_tpc_calibration_study(config, outputFile)
-    if args.run_all or args.run_comparison:         run_comparison_study(config, outputFile, norm_factor)
-    if args.run_all or args.run_closePairRejection: run_close_pair_rejection(config, outputFile)
     
     outputFile.Close()
 
