85d352d7c53dac5fa299f87682a6856bd1baaedf
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2025-06-18 12:24:26 +0200

    updated paths

--- mixed_event/processEM.C
+++ mixed_event/processEM.C
@@ -58,8 +58,8 @@ void processEM(bool doMerge = false)
 
     if (doMerge)
     {
-        //std::string inputFileName = "/data/galucia/lithium_local/same/LHC23_PbPb_pass4_long_same_lsus.root";
-        std::string inputFileName = "/data/galucia/lithium_local/same/LHC24as_pass1_same.root";
+        std::string inputFileName = "/data/galucia/lithium_local/same/LHC23_PbPb_pass4_long_same_lsus.root";
+        //std::string inputFileName = "/data/galucia/lithium_local/same/LHC24as_pass1_same.root";
         //std::string inputFileName = "/Users/glucia/Projects/ALICE/data/lithium/same/LHC24as_pass1_same.root";
         //std::string inputFileName = "/Users/glucia/Projects/ALICE/data/lithium/same/LHC24ag_pass1_skimmed_same.root";
         std::string treeNameCands = "O2he3hadtable";
@@ -146,8 +146,8 @@ void processEM(bool doMerge = false)
     std::cout << "--------------------------------" << std::endl;
 
     Li4Candidate li4CandME;
-    //auto outputFile = TFile::Open("/data/galucia/lithium_local/mixing/LHC23_PbPb_pass4_long_mixing_lsus.root", "RECREATE"); 
-    auto outputFile = TFile::Open("/data/galucia/lithium_local/mixing/LHC24as_pass1_mixing_lsus_new.root", "RECREATE");
+    auto outputFile = TFile::Open("/data/galucia/lithium_local/mixing/LHC23_PbPb_pass4_long_mixing_lsus.root", "RECREATE"); 
+    //auto outputFile = TFile::Open("/data/galucia/lithium_local/mixing/LHC24as_pass1_mixing_lsus_new.root", "RECREATE");
     //auto outputFile = TFile::Open("/Users/glucia/Projects/ALICE/data/lithium/mixing/LHC24ar_pass1_mixing.root", "RECREATE");
     //auto outputFile = TFile::Open("/Users/glucia/Projects/ALICE/data/lithium/mixing/LHC24as_pass1_mixing_small.root", "RECREATE");
     //auto outputFile = TFile::Open("/Users/glucia/Projects/ALICE/data/lithium/same/LHC24ag_pass1_skimmed_mixing.root", "RECREATE");
