da9f7bd70f2a5dde8ac1ea65a084f41cc3f7fcd8
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2025-06-18 12:23:57 +0200

    reorganisation

--- analysis/systematics.py
+++ analysis/systematics.py
@@ -19,88 +19,131 @@ from src.preprocessing import DataPreprocessor
 from studies.correlationStudies import CorrelationStudy
 
 def add_variation(condition: bool, var_name: str, var_value, dataset: Dataset) -> bool:
-    add_condition = True
+    condition = True
     if 'fEta' in var_name:  condition = (abs(dataset[var_name]) < var_value)
     if 'fNSigmaTPC' in var_name:  condition = (abs(dataset[var_name]) < var_value)
     if 'fNSigmaTOFHad' in var_name:  condition = (abs(dataset[var_name]) < var_value) & (abs(dataset['fPtHad']) > 0.8)
 
     return condition
 
+def load_cut_dict(cfgInput):
+    '''
+        Load the cut variation dictionary from the configuration file.
+        The dictionary is expected to have the following structure:
+        {
+            'cut_name': [min_value, max_value, n_steps],'
+            ...
+        }
+    '''
+
+    cut_variation_dict_cfg = cfgInput['cutVariationDict']
+    cut_variation_dict = {}
+    for var_name, var_range in cut_variation_dict_cfg.items():
+        var_min, var_max, n_steps = var_range
+        cut_variation_dict[var_name] = np.linspace(var_min, var_max, n_steps)
+    return cut_variation_dict
+
+def _load_from_file(file_path, tree_names, folder_name=None):
+    '''
+        Load the dataset from a ROOT file.
+        If the folder_name is provided, it will load the dataset from the specified folder.
+        If the folder_name is None, it will load the dataset from the root of the file.
+    '''
+    dataset = None
+    for tree_name in tree_names:
+        if dataset is None:
+            dataset = Dataset.from_root(file_path, folder_name=folder_name, tree_name=tree_name)
+        else:
+            dataset = dataset.concat(Dataset.from_root(file_path, folder_name=folder_name, tree_name=tree_name), axis=1)
+    return dataset
+
+def _drop_columns(dataset, columns_to_keep):
+    '''
+        Drop the columns that are not in the columns_to_keep list.
+    '''
+    columns_to_drop = [col for col in dataset.columns if col not in columns_to_keep]
+    dataset.drop(columns=columns_to_drop, inplace=True)
+    return dataset
+
+def load_data(cfgInput):
+    '''
+        Load the data from the ROOT files and preprocess it.
+    '''
+
+    dataset_SE = _load_from_file(cfgInput['SEFilePath'], cfgInput['SETreeName'], cfgInput.get('SEFolderName', None))
+    
+    process_SE = DataPreprocessor(dataset_SE)
+    process_SE = DataPreprocessor(dataset_SE)
+    process_SE.define_variables()
+    process_SE.define_nsigmaTPC_He3()
+    for selection in cfgInput['selections']:
+        process_SE.apply_cut(selection)
+    
+    dataset_SE = process_SE.dataset
+    dataset_SE = _drop_columns(dataset_SE, cfgInput['columnsToKeep'])
+    print(tc.GREEN+'[INFO]: '+tc.RESET+f'{dataset_SE.columns=}')
+    print(tc.GREEN+'[INFO]: '+tc.RESET+'------------------------------------------------------')
+
+    dataset_ME = _load_from_file(cfgInput['MEFilePath'], cfgInput['METreeName'], cfgInput.get('MEFolderName', None))
+    
+    process_ME = DataPreprocessor(dataset_ME)
+    process_ME.define_variables()
+    process_ME.define_nsigmaTPC_He3()
+    for selection in cfgInput['selections']:
+        process_ME.apply_cut(selection)
+    
+    dataset_ME = process_ME.dataset
+    dataset_ME = _drop_columns(dataset_ME, cfgInput['columnsToKeep'])
+    print(tc.GREEN+'[INFO]: '+tc.RESET+f'{dataset_ME.columns=}')
+    print(tc.GREEN+'[INFO]: '+tc.RESET+'------------------------------------------------------')
+
+    return dataset_SE, dataset_ME
+
+def run_systematic_iteration(dataset_SE, dataset_ME, cut_variation_dict, iter, corr_study):
+    '''
+        Run a single iteration of the systematic analysis.
+        This function is called for each variation of the cuts.
+    '''
+    condition_SE = True
+    condition_ME = True
+    for var_name, cut_values in cut_variation_dict.items():
+        cut_value = np.random.choice(cut_values)
+        condition_SE = condition_SE & add_variation(condition_SE, var_name, cut_value, dataset_SE)
+        condition_ME = condition_ME & add_variation(condition_ME, var_name, cut_value, dataset_ME)
+
+    dataset_SE.add_subset(f'cut_{iter}', condition_SE)
+    dataset_ME.add_subset(f'cut_{iter}', condition_ME)
+
+    hist_SE = dataset_SE.build_th1('fKstar', 
+                                    AxisSpec(40, 0.0, 0.8, f'fKstar_SE_{iter}', '; #it{k}* (GeV/#it{c}); counts'),
+                                    subset=f'cut_{iter}')
+    hist_ME = dataset_ME.build_th1('fKstar', 
+                                    AxisSpec(40, 0.0, 0.8, f'fKstar_ME_{iter}', '; #it{k}* (GeV/#it{c}); counts'),
+                                    subset=f'cut_{iter}')
+
+    corr_study.set_same_event(hist_SE)
+    corr_study.set_mixed_event(hist_ME)
+    corr_study.normalize(low=0.05, high=0.75)
+    hist_corr = corr_study.correlation_function()
+
+    return hist_corr
 
 def systematics(args):
 
     print(tc.GREEN+'[INFO]: '+tc.RESET+'------------------------------------------------------')
-    print(tc.GREEN+'[INFO]: '+tc.BOLD+tc.WHITE+'\t\tRunning analysis'+tc.RESET)
+    print(tc.GREEN+'[INFO]: '+tc.BOLD+tc.WHITE+'\t\tRunning systematics'+tc.RESET)
     print(tc.GREEN+'[INFO]: '+tc.RESET+'------------------------------------------------------')
     print(tc.GREEN+'[INFO]: '+tc.RESET)
 
     with open(args.cfgInputFile, 'r') as file:     
         cfgInput = yaml.safe_load(file)
-    SE_file_path = cfgInput['SEFilePath']
-    SE_tree_name = cfgInput['SETreeName']
-    SE_folder_name = cfgInput.get('SEFolderName', None)
-    ME_file_path = cfgInput['MEFilePath']
-    ME_tree_name = cfgInput['METreeName']
-    ME_folder_name = cfgInput.get('MEFolderName', None)
+    
 
     print(tc.GREEN+'[INFO]: '+tc.RESET+'------------------------------------------------------')
     print(tc.GREEN+'[INFO]: '+tc.RESET+'\t\tCut variation:')
-    # cut variation dictionary: {var_name: [var_min, var_max, n_steps]}
-    cut_variation_dict_cfg = cfgInput['cutVariationDict']
-    cut_variation_dict = {}
-    for var_name, var_range in cut_variation_dict_cfg.items():
-        print(tc.GREEN+'[INFO]: '+tc.RESET+f'{var_name}: {var_range}')
-        var_min, var_max, n_steps = var_range
-        cut_variation_dict[var_name] = np.linspace(var_min, var_max, n_steps)
-    print(tc.GREEN+'[INFO]: '+tc.RESET)
-
-    dataset_SE = None
-    for tree_name in SE_tree_name:
-        if dataset_SE is None:
-            dataset_SE = Dataset.from_root(SE_file_path, folder_name=SE_folder_name, tree_name=tree_name)
-        else:
-            dataset_SE = dataset_SE.concat(Dataset.from_root(SE_file_path, folder_name=SE_folder_name, tree_name=tree_name), axis=1)
-    dataset_SE.query('fPIDtrkHe3 != 6', inplace=True)
-    process_SE = DataPreprocessor(dataset_SE)
-    process_SE.define_variables()
-    process_SE.define_nsigmaTOF_Pr()
-    process_SE.define_kstar()
-    dataset_SE = process_SE.dataset
-    columns_to_keep = [ 'fPtHad',
-                        'fEtaHe3',
-                        'fEtaHad',
-                        'fZVertex',
-                        'fDCAxyHe3',
-                        'fDCAzHe3',
-                        'fDCAxyHad',
-                        'fDCAzHad',
-                        'fNSigmaTPCHe3',
-                        'fNSigmaTPCHad',
-                        'fNSigmaTOFHad',
-                        'fCentralityFT0C',
-                        'fKstar',
-                        'fMassInvLi']
-    columns_to_drop_SE = [col for col in dataset_SE.columns if col not in columns_to_keep]
-    dataset_SE.drop(columns=columns_to_drop_SE, axis=1, inplace=True)
-    print(tc.GREEN+'[INFO]: '+tc.RESET+f'{dataset_SE.columns=}')
-    print(tc.GREEN+'[INFO]: '+tc.RESET+'------------------------------------------------------')
-
-    dataset_ME = None
-    for tree_name in ME_tree_name:
-        if dataset_ME is None:
-            dataset_ME = Dataset.from_root(ME_file_path, tree_name=tree_name)
-        else:
-            dataset_ME = dataset_ME.concat(Dataset.from_root(ME_file_path, tree_name=tree_name), axis=1)
-    dataset_ME.query('fPIDtrkHe3 != 6', inplace=True)
-    process_ME = DataPreprocessor(dataset_ME)
-    process_ME.define_variables()
-    process_ME.define_nsigmaTOF_Pr()
-    process_ME.define_kstar()
-    dataset_ME = process_ME.dataset
-    columns_to_drop_ME = [col for col in dataset_ME.columns if col not in columns_to_keep]
-    dataset_ME.drop(columns=columns_to_drop_ME, inplace=True)
-    print(tc.GREEN+'[INFO]: '+tc.RESET+f'{dataset_ME.columns=}')
-    print(tc.GREEN+'[INFO]: '+tc.RESET+'------------------------------------------------------')
+    
+    cut_variation_dict = load_cut_dict(cfgInput)
+    dataset_SE, dataset_ME = load_data(cfgInput)
 
     hist_corr_funcs = []
     corr_study = CorrelationStudy()
@@ -108,28 +151,8 @@ def systematics(args):
     N_VARIATIONS = cfgInput.get('nVariations', 100)
     with alive_bar(N_VARIATIONS, title=tc.GREEN+'[INFO]: '+tc.RESET) as bar:
         for iter in range(N_VARIATIONS):
-            condition_SE = True
-            condition_ME = True
-            for var_name, cut_values in cut_variation_dict.items():
-                cut_value = np.random.choice(cut_values)
-                condition_SE = condition_SE & add_variation(condition_SE, var_name, cut_value, dataset_SE)
-                condition_ME = condition_ME & add_variation(condition_ME, var_name, cut_value, dataset_ME)
-
-            dataset_SE.add_subset(f'cut_{iter}', condition_SE)
-            dataset_ME.add_subset(f'cut_{iter}', condition_ME)
-
-            hist_SE = dataset_SE.build_th1('fKstar', 
-                                            AxisSpec(40, 0.0, 0.8, f'fKstar_SE_{iter}', '; #it{k}* (GeV/#it{c}); counts'),
-                                            subset=f'cut_{iter}')
-            hist_ME = dataset_ME.build_th1('fKstar', 
-                                            AxisSpec(40, 0.0, 0.8, f'fKstar_ME_{iter}', '; #it{k}* (GeV/#it{c}); counts'),
-                                            subset=f'cut_{iter}')
-
-            corr_study.set_same_event(hist_SE)
-            corr_study.set_mixed_event(hist_ME)
-            corr_study.normalize(low=0.05, high=0.75)
-            hist_corr = corr_study.correlation_function()
-
+            
+            hist_corr = run_systematic_iteration(dataset_SE, dataset_ME, cut_variation_dict, iter, corr_study)
             hist_corr_funcs.append(hist_corr)
             bar()
 
